495800574aac379d1415198de863f2ca5c305ca8
Ricky Pan <rpan@cs.hmc.edu>
2015-11-02 00:43:31 -0800

    deleted string-wrapper

--- chunkystring/string-wrapper.hpp
+++
@@ -1,4 +0,0 @@
-// Use svn rm to get rid of this useless file.  It exists only to catch people
-// not reading the assignment.
-
-#error Please read the assignment and edit LOAD_GENERIC_STRING as instructed.
